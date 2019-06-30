4ad0acee26b2b0b0cd1574be096e844df293d62e
Usama Riaz <usama.riaz@cs.ucla.edu>
2019-06-29 20:30:47 -0700

    Added material class. Added metal support

--- raytrace.py
+++ raytrace.py
@@ -3,6 +3,19 @@ from multiprocessing import Pool
 import numpy as np
 
 
+def random_in_unit_sphere():
+    # Sample from uniform distribution and push values
+    # within the range [-1, 1)
+    loc = 2.0 * np.random.rand(3) - 1.0
+    while np.linalg.norm(loc) >= 1.0:
+        loc = 2.0 * np.random.rand(3) - 1.0
+    return loc
+
+
+def vec3(a, b, c):
+    return np.array([a, b, c])
+
+
 class Ray:
     def __init__(self, origin, direction):
         self.origin = origin
@@ -13,10 +26,11 @@ class Ray:
 
 
 class HitRecord:
-    def __init__(self, t, p, normal):
+    def __init__(self, t, p, normal, material):
         self.t = t
         self.p = p
         self.normal = normal
+        self.material = material
 
 
 class Hitable:
@@ -25,9 +39,10 @@ class Hitable:
 
 
 class Sphere(Hitable):
-    def __init__(self, center, radius):
+    def __init__(self, center, radius, material):
         self.center = center
         self.radius = radius
+        self.material = material
 
     def hit(self, ray, t_min, t_max):
         r = ray.origin - self.center
@@ -53,7 +68,48 @@ class Sphere(Hitable):
             hit_loc = ray.point_at_parameter(hit_t)
             s_norm = hit_loc - self.center
             s_norm /= np.linalg.norm(s_norm)
-            return HitRecord(hit_t, hit_loc, s_norm)
+            return HitRecord(hit_t, hit_loc, s_norm, self.material)
+
+
+class ScatterResult:
+    def __init__(self, scattered_ray, attenuation):
+        self.scattered_ray = scattered_ray
+        self.attenuation = attenuation
+
+
+class Material:
+    def scatter(self, in_ray, attenutation):
+        return
+
+
+class Lambertian(Material):
+    def __init__(self, albedo):
+        self.albedo = albedo
+
+    def scatter(self, in_ray, hit_rec):
+        target = hit_rec.normal + random_in_unit_sphere()
+        scattered_ray = Ray(hit_rec.p, target)
+        return ScatterResult(scattered_ray, self.albedo)
+
+
+class Metal(Material):
+    def __init__(self, albedo, fuzz):
+        self.albedo = albedo
+        self.fuzz = max(1.0, fuzz)
+
+    def scatter(self, in_ray, hit_rec):
+        unit_dir = in_ray.direction / np.linalg.norm(in_ray.direction)
+        ref_r = self.reflect(unit_dir, hit_rec.normal)
+        ref_r += self.fuzz * random_in_unit_sphere()
+
+        if ref_r.dot(hit_rec.normal) <= 0:
+            return
+
+        scattered = Ray(hit_rec.p, ref_r)
+        return ScatterResult(scattered, self.albedo)
+
+    def reflect(self, source, normal):
+        return source - 2 * source.dot(normal) * normal
 
 
 class World:
@@ -76,10 +132,10 @@ class World:
 
 class Camera:
     def __init__(self):
-        self.lower_left_corner = np.array([-2.0, -1.0, -1.0])
-        self.horizontal = np.array([4.0, 0.0, 0.0])
-        self.vertical = np.array([0.0, 2.0, 0.0])
-        self.origin = np.array([0.0, 0.0, 0.0])
+        self.lower_left_corner = vec3(-2.0, -1.0, -1.0)
+        self.horizontal = vec3(4.0, 0.0, 0.0)
+        self.vertical = vec3(0.0, 2.0, 0.0)
+        self.origin = vec3(0.0, 0.0, 0.0)
 
     def get_ray(self, u, v):
         ray_offset = u * self.horizontal + v * self.vertical
@@ -94,30 +150,26 @@ class RayTraceOperation:
         self.world = world
         self.camera = camera
 
-    def random_in_unit_sphere(self):
-        # Sample from uniform distribution and push values
-        # within the range [-1, 1)
-        loc = 2.0 * np.random.rand(3) - 1.0
-        while np.linalg.norm(loc) >= 1.0:
-            loc = 2.0 * np.random.rand(3) - 1.0
-        return loc
-
-    def color(self, ray):
+    def color(self, ray, depth=0):
         MAX_DIST = 1000000000
-        hit_rec = self.world.hit(ray, 0.001, MAX_DIST)
+        hit_rec = self.world.hit(ray, 0.00001, MAX_DIST)
 
         if hit_rec:
-            target = hit_rec.normal + self.random_in_unit_sphere()
-            return .5 * self.color(Ray(hit_rec.p, target))
+            scat_res = hit_rec.material.scatter(ray, hit_rec)
+            if depth < 50 and scat_res:
+                return scat_res.attenuation * self.color(
+                    scat_res.scattered_ray, depth + 1)
+            else:
+                return vec3(0.0, 0.0, 0.0)
 
         norm_vec = ray.direction / np.linalg.norm(ray.direction)
         t = .5 * (norm_vec[1] + 1.0)
-        c1 = np.array([1.0, 1.0, 1.0])
-        c2 = np.array([.5, .7, 1.0])
+        c1 = vec3(1.0, 1.0, 1.0)
+        c2 = vec3(.5, .7, 1.0)
         return (1.0 - t) * c1 + t * c2
 
     def compute_pixel_color(self):
-        col = np.array([0.0, 0.0, 0.0])
+        col = vec3(0.0, 0.0, 0.0)
         for s in range(n_samples_in_pixel):
             u = (self.x + np.random.rand()) / im_width
             v = (self.y + np.random.rand()) / im_height
@@ -125,12 +177,11 @@ class RayTraceOperation:
             r = self.camera.get_ray(u, v)
             col += self.color(r)
 
-        col /= n_samples_in_pixel
-        col = np.sqrt(col)
-        return np.array(255.0 * col, dtype=np.uint8)
+        col = 255.0 * np.sqrt(col / n_samples_in_pixel)
+        return np.array(col, dtype=np.uint8)
 
 
-def f(ray_trace_op):
+def compute_pixel_color(ray_trace_op):
     return ray_trace_op.compute_pixel_color()
 
 
@@ -138,16 +189,30 @@ if __name__ == '__main__':
     scale = 4.0
     base_im_width = 200
     base_im_height = 100
-    n_samples_in_pixel = 10
-    num_processors = 8
+    n_samples_in_pixel = 50
+    num_processors = 16
 
     im_width = int(base_im_width * scale)
     im_height = int(base_im_height * scale)
     im_arr = np.zeros((im_height, im_width, 3), dtype=np.uint8)
 
-    s1 = Sphere(np.array([0.0, 0.0, -1.0]), .5)
-    s2 = Sphere(np.array([0.0, -100.5, -1.0]), 100)
-    world = World([s1, s2])
+    s1 = Sphere(vec3(0.0, 0.0, -1.0),
+                .5,
+                Lambertian(vec3(.8, .3, .3)))
+
+    s2 = Sphere(vec3(0.0, -100.5, -1.0),
+                100,
+                Lambertian(vec3(.8, .8, 0.0)))
+
+    s3 = Sphere(vec3(1.0, 0.0, -1.0),
+                .5,
+                Metal(vec3(.8, .6, .2), .3))
+
+    s4 = Sphere(vec3(-1.0, 0.0, -1.0),
+                .5,
+                Metal(vec3(.8, .8, .8), 1.0))
+
+    world = World([s1, s2, s3, s4])
 
     cam = Camera()
     ray_trace_args = []
@@ -156,10 +221,9 @@ if __name__ == '__main__':
             y = im_height - j
             op = RayTraceOperation(x, y, world, cam)
             ray_trace_args.append(op)
-#            im_arr[j, x] = raytrace(x, y, world, cam)
 
     with Pool(num_processors) as p:
-        cols_per_pixel = p.map(f, ray_trace_args)
+        cols_per_pixel = p.map(compute_pixel_color, ray_trace_args)
         for op, color in zip(ray_trace_args, cols_per_pixel):
             j = im_height - op.y
             im_arr[j, op.x] = color
